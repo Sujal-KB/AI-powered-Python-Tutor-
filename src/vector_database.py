eab4554367e909b5f9524e906f0d23d169779ef6
Sujal-KB <168100533+Sujal-KB@users.noreply.github.com>
2026-03-26 17:56:06 +0530

    Update vector_database.py

--- src/vector_database.py
+++ src/vector_database.py
@@ -21,7 +21,7 @@ def create_vectordb(chunks):
 
     retriever = vector_store.as_retriever(
         search_type="mmr",
-        search_kwargs={"k": 8}
+        search_kwargs={"k": 3}
     )
 
     return retriever
