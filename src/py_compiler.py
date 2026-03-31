7a8ac2b70c52b681ee06c998c0363b11ac7a8158
Sujal-KB <168100533+Sujal-KB@users.noreply.github.com>
2026-03-31 12:39:22 +0530

    Reorganize imports and update shell command format

--- src/py_compiler.py
+++ src/py_compiler.py
@@ -1,13 +1,18 @@
-from langchain_community.tools import ShellTool
+import os
 import warnings
 warnings.filterwarnings('ignore')
 
+from langchain_community.tools import ShellTool
+
+
 def get_py_output(code):
-    
+
     with open("test.py",'w') as file:
         file.write(code)
-
-    shell_tool=ShellTool()
-
-    output=shell_tool.run("python test.py")
+    
+    file_name="test.py"
+    shell_tool = ShellTool()
+    output = shell_tool.run(f"python {file_name}")
     return output
+
+    
